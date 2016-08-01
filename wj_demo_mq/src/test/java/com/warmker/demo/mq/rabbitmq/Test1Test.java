14aa3423af0aecdb53068031e4bfe78486517878
warmker <weamer6611@163.com>
2016-08-01 14:07:38 +0800

    调整目录结构

---
+++ wj_demo_mq/src/test/java/com/warmker/demo/mq/rabbitmq/Test1Test.java
@@ -0,0 +1,22 @@
+package com.warmker.demo.mq.rabbitmq;
+
+import org.junit.After;
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by wendell.wu on 2016/8/1.
+ */
+public class Test1Test {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+}
